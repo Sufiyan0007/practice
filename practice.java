010fc33a92ae1ce4241b4435db0383c9c0c04a27
Sufiyan0007 <sufiyanlaxmeshwar17@gmail.com>
2025-04-11 10:51:36 +0530

    msg

---
+++ practice.java
@@ -0,0 +1,8 @@
+public class practice {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("The Addition is : "+sum);
+        } 
+    }
